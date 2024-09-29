221aed1beb2752ef4a45c74e8e17490b88f87cc5
peter-lyr <llydrp.ldp@gmail.com>
2024-09-30 00:35:04 +0800

    KK[k]

--- plugin/leader_c_g.lua
+++ plugin/leader_c_g.lua
@@ -1,11 +1,16 @@
 local F = require 'f'
 
-local k = {
+if not KK then
+  KK = {}
+end
+
+local k = 'LeaderCGKeys1'
+
+KK[k] = {
   ['<leader><c-g>'] = { name = 'curline', },
-  ['<leader><c-g>n'] = { function() F.norm_font_size() end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
-  ['<leader><c-g>i'] = { function() F.inc_font_size() end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
-  ['<leader><c-g>d'] = { function() F.dec_font_size() end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>n'] = { function() F.w(F.norm_font_size, k) end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>j'] = { function() F.w(F.dec_font_size, k) end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>k'] = { function() F.w(F.inc_font_size, k) end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
 }
 
-require 'which-key'.register(k)
-F.map_buffer_until(k)
+require 'which-key'.register(KK[k])
