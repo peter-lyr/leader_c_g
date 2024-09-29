582c5c84eebd3e3a8fe5fc95a07ea22a99674124
peter-lyr <llydrp.ldp@gmail.com>
2024-09-30 00:35:52 +0800

    F.k_k(F.norm_font_size, k)

--- plugin/leader_c_g.lua
+++ plugin/leader_c_g.lua
@@ -8,9 +8,9 @@ local k = 'LeaderCGKeys1'
 
 KK[k] = {
   ['<leader><c-g>'] = { name = 'curline', },
-  ['<leader><c-g>n'] = { function() F.w(F.norm_font_size, k) end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
-  ['<leader><c-g>j'] = { function() F.w(F.dec_font_size, k) end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
-  ['<leader><c-g>k'] = { function() F.w(F.inc_font_size, k) end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>n'] = { function() F.k_k(F.norm_font_size, k) end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>j'] = { function() F.k_k(F.dec_font_size, k) end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>k'] = { function() F.k_k(F.inc_font_size, k) end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register(KK[k])
