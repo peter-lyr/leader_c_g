ed7e34172fd188468b151cc49df2d0c67634b4a8
peter-lyr <llydrp.ldp@gmail.com>
2024-09-30 01:07:53 +0800

    <leader><c-g>a max_font_size

--- plugin/leader_c_g.lua
+++ plugin/leader_c_g.lua
@@ -5,10 +5,12 @@ local lua = F.getlua(debug.getinfo(1)['source'])
 local k = lua .. 1
 
 KK[k] = {
-  ['<leader><c-g>'] = { name = 'curline', },
+  ['<leader><c-g>'] = { name = 'leader c-g', },
   ['<leader><c-g>n'] = { function() F.k_k(F.norm_font_size, k) end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
   ['<leader><c-g>j'] = { function() F.k_k(F.dec_font_size, k) end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
   ['<leader><c-g>k'] = { function() F.k_k(F.inc_font_size, k) end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>i'] = { function() F.k_k(F.min_font_size, k) end, 'min_font_size', mode = { 'n', 'v', }, silent = true, },
+  ['<leader><c-g>a'] = { function() F.k_k(F.max_font_size, k) end, 'max_font_size', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register(KK[k])
