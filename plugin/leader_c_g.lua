dd41b24bde351a488d90e604f214a45af9ea6027
peter-lyr <llydrp.ldp@gmail.com>
2024-09-29 23:38:00 +0800

    testing F.map_buffer_until(k)

--- plugin/leader_c_g.lua
+++ plugin/leader_c_g.lua
@@ -1,8 +1,11 @@
 local F = require 'f'
 
-require 'which-key'.register {
+local k = {
   ['<leader><c-g>'] = { name = 'curline', },
   ['<leader><c-g>n'] = { function() F.norm_font_size() end, 'norm_font_size', mode = { 'n', 'v', }, silent = true, },
   ['<leader><c-g>i'] = { function() F.inc_font_size() end, 'inc_font_size', mode = { 'n', 'v', }, silent = true, },
   ['<leader><c-g>d'] = { function() F.dec_font_size() end, 'dec_font_size', mode = { 'n', 'v', }, silent = true, },
 }
+
+require 'which-key'.register(k)
+F.map_buffer_until(k)
